7e66d48cd38ac3ce9b78c4c09eefe7872a4fa526
Rajeev Siewnath <rajeev@visuado.com>
2026-05-31 10:16:00 +0300

    step-2-assertions

--- tests/e2e/projects.spec.ts
+++ tests/e2e/projects.spec.ts
@@ -1,5 +1,16 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("homepage loads", async ({ page }) => {
   await page.goto("/");
+
+  await expect(page.getByRole("heading")).toHaveText("Projects");
+
+  // ❌ Bad example:
+  // Checks implementation details.
+  //
+  // expect(await page.content())
+  //   .toContain("Projects");
+
+  // ✅ Good example:
+  // Checks what the user sees.
 });
